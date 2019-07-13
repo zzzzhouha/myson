353d9bae7f0f73ad554d53e3c658545c78435f09
zzzzhouha <zhouzzz47@163.com>
2019-07-13 11:46:01 +0800

    modify

---
+++ console/js/alert.js
@@ -0,0 +1,11 @@
+/**
+ * Created by zhouha on 2019/7/13.
+ */
+function taoLu(){
+    alert("taolu1====");
+    alert("taolu2====");
+    alert("taolu3====");
+}
+alert("你好呀");
+taoLu();
+alert("我是个天才");
